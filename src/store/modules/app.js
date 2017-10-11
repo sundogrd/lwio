67f3575f404960339d58056d51ce4e565827a2d7
lwio <443474713@qq.com>
2017-10-11 16:55:05 +0800

    fuck

---
+++ src/store/modules/app.js
@@ -0,0 +1,16 @@
+const app = {
+  state: {
+    showLogin: false
+  },
+
+  mutations: {
+    TOGGLE_LOGIN_MODAL: (state) => {
+      state.showLogin = !state.showLogin;
+    },
+  },
+
+  actions: {
+  }
+};
+
+export default app;
