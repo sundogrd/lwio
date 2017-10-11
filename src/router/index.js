03593699c5579dc99ded85b516b7187a9f41896d
lwio <443474713@qq.com>
2017-10-11 17:02:30 +0800

    finnally fix the bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 const _import = require('./_import_' + process.env.NODE_ENV);
+
 // in development env not use Lazy Loading,because Lazy Loading large page will cause webpack hot update too slow.so only in production use Lazy Loading
 
 /* layout */
-const Layout = _import('layout/Layout');
-
-const Index = _import('index/index');
+import Layout from 'views/layout/Layout';
+import Index from 'views/index';
 /* login */
-const Login = _import('login/index');
+import Login from 'views/login'
 
 const Blog = _import('blog/blog');
 const Publish = _import('blog/publish');
