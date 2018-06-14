6cae266c931e42a9fc3a072ee75330a212838d11
breakinferno <1972952841@qq.com>
2018-06-14 13:53:43 +0800

    publish start

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import IconSvg from './IconSvg'
+import LFInput from './LFInput'
+import LwCard from './LwCard'
+
+export default {
+  IconSvg, LFInput, LwCard
+}
--- src/views/article/publish.vue
+++ src/views/article/publish.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="publish-container">
     <div class="editor-wrapper">
-      <input id="title" type="text" name="title" placeholder="title" v-model="title">
-      <div class="textinput" contenteditable="true" @keyup="changeText($event)"></div>
-      <footer>
+      <!-- <input id="title" type="text" name="title" placeholder="title" v-model="title"> -->
+      <el-row type="flex" justify="end">
+        <el-button type="success">发布</el-button>
+        <el-button type="warning">草稿</el-button>
+      </el-row>
+      <LFInput name="title" v-model="title" required :maxlength="100">
+        标题
+      </LFInput>
+      <div id="editor">
+        <textarea class="editor-part" :value="content" @input="updateContent" placeholder="输入markdown语法"></textarea>
+        <div class="editor-part" v-html="compiledMarkdown"></div>
+      </div>
+      <!-- <footer>
         <button class="lwio-btn" @click="submitBlog">Publish</button>
         <label for="publishToken">
           Publish Token
           <input id="publishToken" type="password" name="publishToken" v-model="publishToken">
         </label>
-      </footer>
+      </footer> -->
     </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
+import marked from 'marked';
+// import LFInput from 'components/LFInput';
 import { mapGetters } from 'vuex';
-import { postArticles } from 'api/article'
+import { postArticles } from 'api/article';
 export default {
   name: 'publish',
   data() {
@@ -24,14 +37,19 @@ export default {
       publishToken: '',
       title: '',
       content: ''
-    }
+    };
   },
   computed: {
-    ...mapGetters([
-      'user'
-    ])
+    compiledMarkdown() {
+      return marked(this.content, { sanitize: true });
+    },
+    ...mapGetters(['user'])
   },
   methods: {
+    updateContent: _.debounce(function(e) {
+      debugger
+      this.content = e.target.value;
+    }, 300),
     changeText(event) {
       this.content = event.target.innerText;
     },
@@ -43,8 +61,11 @@ export default {
         });
       }
       try {
-        const article = await postArticles({ title: this.title, content: this.content })
-        this.$router.push({ path: `/article/${article.id}` })
+        const article = await postArticles({
+          title: this.title,
+          content: this.content
+        });
+        this.$router.push({ path: `/article/${article.id}` });
       } catch (e) {
         this.$notify.error({
           title: '错误',
@@ -53,12 +74,28 @@ export default {
       }
     }
   }
-}
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
+.publish-container {
+  height: 100%;
+  flex: 1;
+  align-items: stretch;
+
   .editor-wrapper {
-    margin: 100px;
+    padding: 40px 100px 0px;
+    /* height: 100%; */
+    min-height: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: auto;
+    right: 0;
+    box-sizing: border-box;
+    left: 0;
+    display: flex;
+    flex-direction: column;
 
     .textinput {
       height: 60px;
@@ -85,5 +122,44 @@ export default {
       }
     }
   }
-  
+}
+
+#editor {
+  margin: 0;
+  height: 100%;
+  font-family: "Helvetica Neue", Arial, sans-serif;
+  color: #333;
+  display: flex;
+  flex-grow: 1;
+
+  .editor-part {
+    align-items: stretch;
+  }
+}
+
+textarea,
+#editor div {
+  display: inline-block;
+  width: 49%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 0 20px;
+}
+
+textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: "Monaco", courier, monospace;
+  padding: 20px;
+  background: rgba(110, 141, 144, 0.5);
+}
+
+code {
+  color: #f66;
+}
 </style>
