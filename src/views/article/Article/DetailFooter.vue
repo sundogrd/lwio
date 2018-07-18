6c79762d3584f7c59c981b9a865d0c71e01d4361
lwyj123 <443474713@qq.com>
2018-07-18 12:42:59 +0800

    feat: plan to modify article detail page into SSR

---
+++ src/views/article/Article/DetailFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="detail-footer">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailFooter',
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .detail-footer {
+
+  }
+</style>
