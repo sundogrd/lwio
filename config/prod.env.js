a59515a302d52bcd1b83be84c985719e84579726
lwyj123 <443474713@qq.com>
2018-11-30 17:50:51 +0800

    feat: vue-cli updated

--- config/prod.env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    NODE_ENV: '"production"',
-    BASE_API: '"http://lwio.me/api/"',
-    APP_ORIGIN: '"http://wuanlife.com"',
-    // 七牛域名
-    QINIU_DOMAIN_URL: '"http://7xlx4u.com1.z0.glb.clouddn.com/"'
-}
