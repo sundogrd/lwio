c0e87a4ffdd93a26d94249789f700971b0f99f95
liangyuyi <liangyuyi@bytedance.com>
2019-01-28 21:08:40 +0800

    feat: add gzip

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,26 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+const productionGzipExtensions = ['js', 'css']
+
 module.exports = {
-  configureWebpack: {
-    devServer: {
-        disableHostCheck: true,
-        clientLogLevel: 'info',
-        watchOptions: {
-            poll: true
-        }
+  configureWebpack: config => {
+    config.devServer = {
+      disableHostCheck: true,
+      clientLogLevel: 'info',
+      watchOptions: {
+          poll: true
+      }
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      // 生产环境build gzip
+      config.plugins.push(new CompressionWebpackPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      }))
     }
   },
 
