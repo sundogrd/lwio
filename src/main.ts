2a0d68e5d037c3e8d14ca301e06f0015607facc8
lwyj123 <liang.pearce@gmail.com>
2019-03-03 16:45:09 +0800

    feat: 发文页自动跳转

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import './plugins/element.js'
+import NProgress from 'nprogress'; // Progress 进度条
+import 'nprogress/nprogress.css';// Progress 进度条 样式
 import * as authService from './services/auth';
+import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from 'constants';
 
 Vue.config.productionTip = false
 
@@ -28,13 +31,25 @@ const neededLoginList = ['articlePublishPage']// need redirect list
 
 router.beforeEach((to, from, next) => {
   hasLogined().then(isLogined => {
-    if (neededLoginList.indexOf(to.name || '') !== -1) {
+    if (neededLoginList.indexOf(to.name || '') === -1) {
       next()
+      SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION
     }
     if (isLogined) {
       next()
+      return
     } else {
       next("/api/oauth2/github/login")
+      return
     }
   })
 })
+
+router.beforeEach((to, from, next) => {
+  NProgress.start(); // 开启Progress
+  next()
+});
+
+router.afterEach(() => {
+  NProgress.done(); // 结束Progress
+});
