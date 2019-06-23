c46f2563e8bf48680a36677b31a100303a6af9dd
lwyj123 <liang.pearce@gmail.com>
2019-06-23 15:21:49 +0800

    feat: 七牛图片上传并修复header的bug

--- src/views/content/article/article-publish-page/index.vue
+++ src/views/content/article/article-publish-page/index.vue
@@ -74,130 +74,129 @@ export default class ArticlePublishPage extends Vue {
   padding-right: 20px;
 }
 
-.publish-page{
-    header{
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        padding: 0 20px;
-        height: 64px;
-        background-color: #fff;
-        border-bottom: none;
-        z-index: 100;
-        max-width: 1000px;
-        margin: 0 auto;
-    }
-    .publish-title{
-        padding: 0;
-        font-size: 28px;
-        font-weight: 700;
-        color: #000;
-        border: none;
-        outline: none;
-        resize: none;
-        overflow: hidden;
-        position: relative;
-        max-width: 856px;
-        margin: 0 auto;
-        left: 50%;
-        transform: translateX(-50%);
-        padding-left: 40px;
-        top: 116px;
-        height: initial;
-        padding-top: 10px;
-        padding-bottom: 10px;
-    }
-    .publish-header{
-        display: flex;
-        align-items: center;
-        margin: 0 auto;
-        width: 83.334rem;
-        max-width: 100%;
+.publish-page {
+  header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding: 0 20px;
+    height: 64px;
+    background-color: #fff;
+    border-bottom: none;
+    z-index: 100;
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+  .publish-title {
+    padding: 0;
+    font-size: 28px;
+    font-weight: 700;
+    color: #000;
+    border: none;
+    outline: none;
+    resize: none;
+    overflow: hidden;
+    position: relative;
+    max-width: 856px;
+    margin: 0 auto;
+    left: 50%;
+    transform: translateX(-50%);
+    padding-left: 40px;
+    top: 116px;
+    height: initial;
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+  .publish-header {
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    width: 83.334rem;
+    max-width: 100%;
+    height: 100%;
+
+    .avatar {
+      width: 32px;
+      height: 32px;
+      border-radius: 50%;
+      margin-left: 30px;
+      img {
+        width: 100%;
         height: 100%;
-
-        .avatar{
-            width: 32px;
-            height: 32px;
-            border-radius: 50%;
-            margin-left: 30px;
-            img{
-                width: 100%;
-                height: 100%;
-                border-radius: inherit;
-            }
-        }
-        .logo{
-            position: relative;
-            margin: 0 auto 0 0;
-            width: 3.067rem;
-            height: 3.067rem;
-            background: 50% no-repeat;
-            background-size: contain;
-            background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAMAAAC5zwKfAAAAjVBMV…RS3bfRSan1waGZMVXwctmrrKdPqhgt0jnVtBa+DwMDA3r8A41Ik44EoNuxAAAAAElFTkSuQmCC);
-        }
-        .status-text{
-            font-size: 18px;
-            white-space: nowrap;
-            color: #ddd;
-            cursor: default;
-            user-select: none;
-
-            a{
-                margin: 0 0 0 6px;
-                padding: 3px 6px;
-                color: inherit;
-                text-decoration: none;
-                border: 1px solid currentColor;
-                border-radius: 2px;
-            }
-        }
-        .header-operations{
-            user-select: none;
-            margin-left: 60px;
-            .operation-btn{
-                font-size: 20px;
-                white-space: nowrap;
-                color: #007fff;
-                cursor: pointer;
-                user-select: none;
-                span{
-                    font-size: 18px;
-                    white-space: nowrap;
-                    color: #007fff;
-                    cursor: pointer;
-                    user-select: none;
-                }
-                .icon{
-                    margin-left: 6px;
-                }
-            }
-        }
+        border-radius: inherit;
+      }
     }
-    .publish-editor{
-        margin-top: 120px;
-        .ProseMirror{
-            height: calc(100vh - 184px);
-        }
-        .ProseMirror-content{
-            margin-bottom: 0px;
-            padding-bottom: 0px;
-            padding-top: 0px;
-            // margin-top: 30px;
-            &::-webkit-scrollbar{
-                display: none;
-            }
-        }
+    .logo {
+      position: relative;
+      margin: 0 auto 0 0;
+      width: 3.067rem;
+      height: 3.067rem;
+      background: 50% no-repeat;
+      background-size: contain;
+      background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAMAAAC5zwKfAAAAjVBMV…RS3bfRSan1waGZMVXwctmrrKdPqhgt0jnVtBa+DwMDA3r8A41Ik44EoNuxAAAAAElFTkSuQmCC);
+    }
+    .status-text {
+      font-size: 18px;
+      white-space: nowrap;
+      color: #ddd;
+      cursor: default;
+      user-select: none;
 
-        .ProseMirror-menubar{
-            position: fixed;
-            top: 76px;
-            z-index: 100;
-            max-width: 856px;
-            margin: 0 auto;
-            padding: 0 1rem 0 2rem;
+      a {
+        margin: 0 0 0 6px;
+        padding: 3px 6px;
+        color: inherit;
+        text-decoration: none;
+        border: 1px solid currentColor;
+        border-radius: 2px;
+      }
+    }
+    .header-operations {
+      user-select: none;
+      margin-left: 60px;
+      .operation-btn {
+        font-size: 20px;
+        white-space: nowrap;
+        color: #007fff;
+        cursor: pointer;
+        user-select: none;
+        span {
+          font-size: 18px;
+          white-space: nowrap;
+          color: #007fff;
+          cursor: pointer;
+          user-select: none;
         }
+        .icon {
+          margin-left: 6px;
+        }
+      }
+    }
+  }
+  .publish-editor {
+    margin-top: 120px;
+    .ProseMirror {
+      height: calc(100vh - 184px);
+    }
+    .ProseMirror-content {
+      margin-bottom: 0;
+      padding-bottom: 0;
+      padding-top: 0;
+      // margin-top: 30px;
+      &::-webkit-scrollbar {
+        display: none;
+      }
     }
 
+    .ProseMirror-menubar {
+      position: fixed;
+      top: 76px;
+      z-index: 100;
+      max-width: 856px;
+      margin: 0 auto;
+      padding: 0 1rem 0 2rem;
+    }
+  }
 }
 </style>
